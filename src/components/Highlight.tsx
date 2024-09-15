611a15f6cd0c242086a81ddcd1c90b020512ffdf
Fabiano Braga <fabianobragadev@gmail.com>
2024-09-15 08:39:24 -0300

    feature/finish-higlight

---
+++ src/components/Highlight.tsx
@@ -0,0 +1,19 @@
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export function Highlight({ title, subtitle }: Props) {
+  return (
+    <Center>
+      <Text fontFamily="$heading" fontSize="$4xl" color="$gray50">
+        {title}
+      </Text>
+      <Text fontFamily="$body" color="$gray200" fontSize="$lg">
+        {subtitle}
+      </Text>
+    </Center>
+  );
+}
