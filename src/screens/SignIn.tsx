a9bd7859008afefb41a1f77677181e24b0d86774
Fabiano Braga <fabianobragadev@gmail.com>
2024-09-17 12:00:33 -0300

    finish screen login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
-import { Center, HStack, Image, Text, VStack } from "@gluestack-ui/themed";
+import { Center, HStack, Icon, Image, Text } from "@gluestack-ui/themed";
 
 import Logo from "@assets/logo.png";
+
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { Eye } from "phosphor-react-native";
 
 export function SignIn() {
   return (
@@ -14,10 +16,17 @@ export function SignIn() {
         Acesse sua conta
       </Text>
       <Input placeholder="E-mail" />
-      <HStack>
-        <Input placeholder="Senha" />
-      </HStack>
+      <Input placeholder="Senha" />
       <Button title="Entrar" />
+
+      <Text mt="$24" color="$gray100" fontSize="$lg" fontFamily="$heading">
+        Ainda não tem acesso?
+      </Text>
+      <Button
+        title="Criar uma conta"
+        buttonVariant="basic"
+        buttonVariantText="secondary"
+      />
     </Center>
   );
 }
