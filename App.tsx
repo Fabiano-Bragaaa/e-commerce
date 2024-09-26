68ceafcd4c76aa506b0664955c69ac880c00ba0c
Fabiano Braga <fabianobragadev@gmail.com>
2024-09-26 13:22:42 -0300

    config and implementations the public routes

--- App.tsx
+++ App.tsx
@@ -7,15 +7,15 @@ import {
 } from "@expo-google-fonts/karla";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
-import { SignIn } from "@screens/SignIn";
-import { SignUp } from "@screens/SignUp";
+
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
 
   return (
     <GluestackUIProvider config={config}>
-      {fontsLoaded ? <SignIn /> : <View />}
+      {fontsLoaded ? <Routes /> : <View />}
     </GluestackUIProvider>
   );
 }
