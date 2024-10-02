e7981d25af0fce1f7bfbde143b303439c5e85d25
Fabiano Braga <fabianobragadev@gmail.com>
2024-10-02 10:28:26 -0300

    select component done

---
+++ src/components/SelectFilter.tsx
@@ -0,0 +1,48 @@
+import {
+  Box,
+  ChevronDownIcon,
+  Icon,
+  Select,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectInput,
+  SelectItem,
+  SelectPortal,
+  SelectTrigger,
+} from "@gluestack-ui/themed";
+import { useState } from "react";
+
+export function SelectFilter() {
+  const [valueSelected, setValueSelected] = useState("todos");
+
+  console.log(valueSelected);
+
+  return (
+    <Select
+      w={"40%"}
+      initialLabel="Todos"
+      defaultValue={valueSelected}
+      onValueChange={setValueSelected}
+    >
+      <SelectTrigger variant="outline" size="md">
+        <SelectInput />
+        <Box mr="$3">
+          <Icon as={ChevronDownIcon} />
+        </Box>
+      </SelectTrigger>
+      <SelectPortal>
+        <SelectBackdrop />
+        <SelectContent>
+          <SelectDragIndicatorWrapper>
+            <SelectDragIndicator />
+          </SelectDragIndicatorWrapper>
+          <SelectItem label="Todos" value="todos" />
+          <SelectItem label="Novos" value="novos" />
+          <SelectItem label="Usados" value="usados" />
+        </SelectContent>
+      </SelectPortal>
+    </Select>
+  );
+}
