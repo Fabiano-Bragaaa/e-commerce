130b80c71022b51d2ef1351287e935dabadf32b8
Fabiano Braga <fabianobragadev@gmail.com>
2024-10-11 09:56:52 -0300

    config ads screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -8,10 +8,40 @@ import { InputHome } from "@components/InputHome";
 import { MyAdsCard } from "@components/MyAdsCard";
 import { Ads } from "@components/Ads";
 import { ModalContent } from "@components/ModalContent";
+import { AdsTypeDTO } from "@dtos/Ads";
+import { useNavigation } from "@react-navigation/native";
+import { AppRoutesNavigationProps } from "@routes/app.routes";
 
 export function Home() {
-  const [ads, setAds] = useState(["1", "2", "3", "4", "5", "6", "7", "8"]);
+  const [ads, setAds] = useState<AdsTypeDTO[]>([
+    {
+      id: "1",
+      title: "Tenis vermelho",
+      subtitle: "45,90",
+    },
+    {
+      id: "2",
+      title: "Tenis preto",
+      subtitle: "55,90",
+    },
+    {
+      id: "3",
+      title: "Tenis azul",
+      subtitle: "35,90",
+    },
+    {
+      id: "4",
+      title: "Tenis amarelo",
+      subtitle: "25,90",
+    },
+  ]);
   const [open, setOpen] = useState(false);
+
+  const navigation = useNavigation<AppRoutesNavigationProps>();
+
+  function handleOpenAds(id: string) {
+    navigation.navigate("ads");
+  }
 
   return (
     <VStack flex={1} p="$3">
@@ -26,8 +56,10 @@ export function Home() {
       <InputHome placeholder="Buscar anúncio" openModal={() => setOpen(true)} />
       <FlatList
         data={ads}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <Ads />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Ads onPress={() => handleOpenAds(item.id)} data={item} />
+        )}
         numColumns={2}
         columnWrapperStyle={{ justifyContent: "space-between" }}
         contentContainerStyle={{ paddingBottom: 14 }}
