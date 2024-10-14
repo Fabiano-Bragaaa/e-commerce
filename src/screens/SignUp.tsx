077424a9469340806368a660966fbda8fe8015c0
Fabiano Braga <fabianobragadev@gmail.com>
2024-10-14 17:31:54 -0300

    config and implementation, toast and image picker is done

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,11 @@
-import { Center, Icon, Image, Text } from "@gluestack-ui/themed";
+import {
+  Center,
+  Icon,
+  Image,
+  Text,
+  Button as ButtonGluestack,
+  useToast,
+} from "@gluestack-ui/themed";
 
 import Logo from "@assets/logo.png";
 import Avatar from "@assets/Avatar.png";
@@ -7,14 +14,65 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { UserPhoto } from "@components/UserPhoto";
-import { ScrollView } from "react-native";
+import { Alert, ScrollView } from "react-native";
 
 import { PencilLine } from "lucide-react-native";
 
+import { useState } from "react";
+
 import { useNavigation } from "@react-navigation/native";
 
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { ToastMessage } from "@components/ToastMessage";
+
 export function SignUp() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/Fabiano-Bragaaa.png"
+  );
   const navigation = useNavigation();
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        allowsEditing: true,
+        aspect: [4, 4],
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoURI = photoSelected.assets[0].uri;
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 1) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Essa imagem é muito grande. Escolha uma de até 5MB."
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log("erro ao enviar a foto", error);
+    }
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -27,8 +85,8 @@ export function SignUp() {
           subtitle="Crie sua conta e use o espaço para comprar itens variados e vender seus produtos"
         />
         <Center w="$full">
-          <UserPhoto source={Avatar} alt="foto de perfil" />
-          <Center
+          <UserPhoto source={{ uri: userPhoto }} alt="foto de perfil" />
+          <ButtonGluestack
             h="$11"
             w="$11"
             rounded="$full"
@@ -36,9 +94,12 @@ export function SignUp() {
             position="absolute"
             right={120}
             top={50}
+            justifyContent="center"
+            alignItems="center"
+            onPress={handleUserPhotoSelect}
           >
             <Icon as={PencilLine} color="$white" size="md" />
-          </Center>
+          </ButtonGluestack>
         </Center>
 
         <Input placeholder="Nome" />
